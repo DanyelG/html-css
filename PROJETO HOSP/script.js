cc2719fd4b7055e91b20060c9b213210145eacc8
DanyelG <134244537+DanyelG@users.noreply.github.com>
2023-11-03 18:22:48 -0300

    news: homepage
    homepage finalizada

--- PROJETO HOSP/script.js
+++ PROJETO HOSP/script.js
@@ -1,16 +1,36 @@
-let count = 1;
-document.getElementById("radio1").cheked = true;
+const controls =
+document.querySelectorAll('.control');
+let currentItem = 0;
+const items = 
+document.querySelectorAll('.item');
+const maxItems = items.length;
 
-setInterval(function(){
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('arrow-left');
+        
+        if(isLeft){
+            currentItem -= 1;
+        }else{
+            currentItem +=1;
+        }
 
-}, 5000)
+        if (currentItem >= maxItems){
+            currentItem = 0;
+        }
 
-function nextImage(){
-    count++;
-    if(count>3){
-    count=1;
-    }
+        if (currentItem < 0){
+            currentItem = maxItems - 1;
+        }
 
-    document.getElementById("radio"+count).cheked = true;
-}
+        items.forEach(item =>
+        item.classList.remove('current-item'));
+            
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior: "smooth"
+        });
 
+        items[currentItem].classList.add("current-item");
+    });
+});
